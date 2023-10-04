966d9f6e6ff29a262a260c4368cf2456de537aa5
bruno <bruno.cezario@agrotools.com.br>
2023-10-04 00:38:48 -0300

    exclusaõ de pasta de aula a mais

--- aula0310/response.php
+++
@@ -1,8 +0,0 @@
-<?php
-function response($error, $message){
-    header('Content-type: application/json');
-    echo json_encode((object) ["error"=>$error, "message"=>$message], JSON_UNESCAPED_UNICODE);
-    die();
-}
-
-?>
